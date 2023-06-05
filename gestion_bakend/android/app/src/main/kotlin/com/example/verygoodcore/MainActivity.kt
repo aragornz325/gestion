408a95eec953ed58e3e75cef9ecc1f9c005e157b
rodrigo quintero <rodrigo.m.quintero@gmail.com>
2023-06-05 16:27:36 -0300

    :construction: inciando repo

---
+++ gestion_bakend/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.gestion_bakend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gestion_bakend/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:gestion_bakend/app/app.dart';
+import 'package:gestion_bakend/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
